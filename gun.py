5c2381c845b064cb5df907879a93e5bc1f599d53
dasdsha <stepanova.dv@phystech.edu>
2020-10-23 10:06:00 +0300

    The base of the game

---
+++ gun.py
@@ -0,0 +1,46 @@
+import pygame as pg
+
+SCREEN_SIZE = (800, 600)
+BLACK = (0, 0, 0)
+pg.init()
+
+class Ball():
+    pass
+
+class Gun():
+    pass
+
+class Table():
+    pass
+
+class Target():
+    pass
+
+class Manager():
+    def __init__(self):
+        self.gun = Gun()
+        self.table = Table()
+
+    def process(self, events, screen):
+        done = self.handle_events(events)
+        self.draw(screen)
+        return done
+    
+    def draw(self, screen):
+        screen.fill(BLACK)
+
+    def handle_events(self, events):
+        done = False
+        for event in events:
+            if event.type == pg.QUIT:
+                done = True
+
+        return done
+
+screen = pg.display.set_mode(SCREEN_SIZE)
+pg.display.set_caption("The gun of Khiryanov")
+clock = pg.time.Clock()
+
+mgr = Manager()
+
+pg.quit()
