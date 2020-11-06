e109852b633213ee0fa1afd0196377c6ef02a125
dasdsha <stepanova.dv@phystech.edu>
2020-11-06 10:43:40 +0300

    Fixing

--- branches/branch_test.py
+++ branches/branch_test.py
@@ -1,10 +1,7 @@
 class featureClass():
     def __init__(self):
-<<<<<<< HEAD
+
         print("Hello")
-=======
-        pass
->>>>>>> 48f6bae0616dadbf75c5791e6d2f0f9085a2e41f
 
     def feature():
         print("My new feature")
